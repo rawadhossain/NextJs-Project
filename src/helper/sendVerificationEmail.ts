8dafd44d6f468d57e6cee82b2f539608a530d94c
rawadhossain <rawad.hossain00@gmail.com>
2024-12-16 21:10:10 +0600

    verify unique username

--- src/helper/sendVerificationEmail.ts
+++ src/helper/sendVerificationEmail.ts
@@ -9,7 +9,7 @@ export async function sendVerificationEmail(
 ): Promise<ApiResponse> {
     try {
         await resend.emails.send({
-            from: 'abcd@example.com',
+            from: 'rwdhossain@gmail.com',
             to: email,
             subject: 'OTP - Verfication Code',
             react: VerificationEmail({ username, otp: verifyCode }),
